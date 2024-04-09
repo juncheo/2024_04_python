ae8a1ed74163c27af2e68356b6b109824db8849c
juncheo <166504687+juncheo@users.noreply.github.com>
2024-04-09 21:41:30 +0900

    Add files via upload

---
+++ day01/input.py
@@ -0,0 +1,19 @@
+# print: 글 나오도록 하는 기능
+# input: 글을 입력 하는 기능
+#drama = input("당신의 최애 드라마는?")
+#print(f"당신의 최애 드라마는 {drama}이군요!")
+
+#name =input("당신의 이름은?")
+#print(f"당신의 이름은 {name}이군요!")
+
+
+#name =input("당신의 이름은?")
+#age =input("당신의 나이는?")
+#color= input("당신이 좋아하는 색깔은?")
+
+#print(f"당신의이름은{name}이고, 당신의 나이는 {age}이고, 당신이 좋아하는 색깔은{color} 이군요!"  )
+
+#day =input("오늘의 날짜는?")
+#program =input("계획하고있는 프로그램은?")
+
+#print(f"오늘의날짜는{day}이고 계획하고있는 프로그램은{program}입니다.")
---
+++ day01/variable.py
@@ -0,0 +1,14 @@
+# 변수(variable)
+# 오른쪽 데이터를 왼쪽의 변수에 넣는다.
+name = "김준철"
+major = "영상컨텐츠학과"
+print(name)
+print(major)
+fruit = "사과"
+print(fruit)
+
+# f문자열 문법
+print(f"저는 {name} 입니다.")
+#저의 이름은 ~ 저의 전공은~ 제가 좋아하는 과일은~입니다.
+print(f"저의 이름은 {name} 저의 전공은 {major} 제가 좋아하는 과일은 {fruit} 입니다.")
+
