edef297604f9e11c5bcd1bd8fbd8809ea345af85
juncheo <166504687+juncheo@users.noreply.github.com>
2024-04-11 21:51:14 +0900

    Add files via upload

---
+++ day02/review_quiz.py
@@ -0,0 +1,15 @@
+# 1. 어제의 일기 프로그램
+
+# date = input("어제의 날짜는?")
+# plan = input("계획은?")
+# print(f"날짜는{date}고 계획은 {plan}이다")
+
+
+# 2. 먹은 메뉴보여주기 프로그램
+
+# date = input("오늘의 날짜는?")
+# breakfast = input("아침식사는?")
+# lunch = input("점심식사는?")
+# dinner = input("저녁식사는?")
+
+# print(f"오늘은날짜는 {date}이고 아침메뉴는 {breakfast}고 점심은{lunch}고 저녁은 {dinner} 입니다")
