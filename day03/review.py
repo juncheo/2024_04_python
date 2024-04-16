25e4dadff990ed3817c6d51c60068518f82f68c5
juncheo <166504687+juncheo@users.noreply.github.com>
2024-04-16 21:46:49 +0900

    Add files via upload

---
+++ day03/review.py
@@ -0,0 +1,21 @@
+#print & 변수
+print("hello world")
+today = "20-04-16"
+print(f"오늘은 {today}")
+
+#input
+food = input("저녁 먹은 음식:")
+print(f"오늘 저녁은 {food}를 먹었군요")
+
+#data type
+#data type casting = int(), str(), float(), bool()
+test = 1  # 정수형 타입
+test1 = "1"  # 문자형 타입
+test3 = true # 불리언형 타입
+test4 = None # None형 타입
+test5 = int(input("숫자 입력:"))
+
+#operator - arith math operetor(산술 연산자)
+# +,-,*,/,//,**,%
+
+
