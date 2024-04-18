62b2233546ad546a1272cdacab07b38ed72940d0
juncheo <166504687+juncheo@users.noreply.github.com>
2024-04-18 21:42:54 +0900

    Add files via upload

---
+++ day04/condition_elif.py
@@ -0,0 +1,53 @@
+# condition_elif
+# num = int(input("점수 입력:"))
+#
+# if num >= 90:
+#     print("A등급")
+# elif num >= 80:
+#     print("B등급")
+# elif num >= 70:
+#     print("C등급")
+# else:
+#     print("과락 입니다.")
+
+
+
+
+
+
+
+# 국, 영, 수 점수를 3개 입력 받고, 평균아 90점이상 'A' 80점 이상 'B' 70점 이상 'C' 60점 이상'D'
+# 나머지는 'F'로 나타내는 프로그램 작성
+
+# X = int(input("국어 점수:"))
+# y = int(input("영어 점수:"))
+# z = int(input("수학 점수:"))
+#
+# sum = (X + y + z) / 3
+#
+# if sum >= 90:
+#     print("A등급")
+# elif sum >= 80:
+#     print("B등급")
+# elif sum >= 70:
+#     print("C등급")
+# elif sum >= 60:
+#     print("D등급")
+# else:
+#     print("F등급")
+
+
+# # nested condition
+# score = int(input("점수 입력:"))
+#
+# if score > 60
+#     if score == 100
+#         print("만점 입니다.")
+#     else:
+#         print("합격입니다.")
+# else:
+#     if score == 0:
+#         print("이건 좀...")
+#     else:
+#         print("불합격입니다.")
+
---
+++ day04/loop.py
@@ -0,0 +1,39 @@
+# 반복문(loop)
+# for 반복문
+# print,input, str, int, float, bool, type
+# range(n): 0~n-1까지 순회해줘
+# for 미지수 in range(n번):
+# for x in range(10000):
+#     print("hello")
+
+# for x in range(10):
+#     print(f"{x}. hello world!")
+
+# num = int(input("몇번 실행할까요?"))
+# for x in range(num):
+#     print(f"{x}. hello world!")
+
+# 0~200까지 나오는 프로그램
+# num = int(input("숫자 입력:"))
+# for x in range(num):
+#     print(x)
+
+# 0~200까지 짝수만 나오는 프로그램
+
+# for x in range(201):
+#     if x % 2 == 0:
+#         print(x)
+
+# 0~1000 3의 배수만 찍히도록 해주세요!
+
+# for x in range(1001):
+#     if x % 3 == 0:
+#         print(x)
+
+
+# 구구단
+# 유저에게 몇단을 입력하면 해당 단을 모두 보여주는 프로그램
+
+# num =int(input("숫자 입력:"))
+# for x in range(10):
+#     print(num * x)
